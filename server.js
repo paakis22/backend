a773904977c491d2ce8cc5b3ad6306a4a9d5e4d9
paakis22 <sripakis@gmail.com>
2025-06-05 23:12:58 +0530

    backend done

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
+
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import authRoutes from './routes/authRoutes.js';
+import cors from 'cors';
 
 dotenv.config();
-const app = express();
-app.use(express.json());
 
+const app = express(); 
 connectDB();
+
+app.use(express.json());
+app.use(cors());
 
 app.use('/api/auth', authRoutes);
 
+app.get('/', (req, res) => {
+  res.send('Time Pro API is running...');
+});
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+
